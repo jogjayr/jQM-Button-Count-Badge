c7c72dc7730a12c6516818f4740309fc4865a33e
jayraj <jogjayr@gmail.com>
2011-10-28 04:14:07 -0400

    first commit of jquery/jqm button badge plugin

---
+++ badge.js
@@ -0,0 +1,23 @@
+(function($) {
+    "use strict";
+    $.fn.mobileBadge = function(options) {
+        if(typeof options.count !== "number") {
+            throw "Specified count isn't a number";
+        }
+        if(options.position !== "topleft" && options.position !== "topright") {
+            throw options.position + " is not a valid position for the count badge. Specify 'topleft' or 'topright'";
+        }
+        var count = options.count,
+            position = options.position,
+            badgeMarkup = "";
+        if(position === "topright" ) {
+            badgeMarkup = "<span class='count_badge badge_position_right'><span class='badge_count'>"+count+"</span></span>";
+        }
+        else {
+            badgeMarkup = "<span class='count_badge badge_position_left'><span class='badge_count'>"+count+"</span></span>";
+        }
+            this.children(".count_badge").remove();
+            this.append(badgeMarkup); 
+        
+   };
+}(jQuery));
