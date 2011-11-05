538f8dab1c92174d646b7a898968ba780abd866e
jayraj <jogjayr@gmail.com>
2011-11-04 22:11:05 -0400

    added support for custom class styling for badges

--- badge.js
+++ badge.js
@@ -9,23 +9,42 @@
         }
         
         var settings = {
-				position: "topright"
+				position: "topright",
+				classnames : "badge_variable"
 			},
 			badgeMarkup = "",
-			attachToElement = this;
+			attachToElement = this, 
+			i = 0;
 		if(options) {
 			$.extend(settings, options);
 		}
+		
+		//Allow setting custom classes for styling the badge
+		if(typeof settings.classnames === "string") {
+			badgeMarkup = "<span class='badge_fixed " + settings.classnames;
+		}
+		//If an array of class names is passed in
+		else if(typeof settings.classnames === "object") {
+			badgeMarkup = "<span class='badge_fixed ";
+			for(i = 0; i < settings.classnames.length; i++) {
+				if(typeof settings.classnames[i] === "string") {
+					badgeMarkup += " " + settings.classnames[i] + " ";
+				}
+			}
+		}
+		
         if(settings.position === "topright" ) {
-            badgeMarkup = "<span class='count_badge badge_position_right'><span class='badge_count'>" + settings.count + "</span></span>";
+            badgeMarkup += " badge_position_right'><span class='badge_count'>" + settings.count + "</span></span>";
         }
+        
         else {
-            badgeMarkup = "<span class='count_badge badge_position_left'><span class='badge_count'>" + settings.count + "</span></span>";
+            badgeMarkup += " badge_position_left'><span class='badge_count'>" + settings.count + "</span></span>";
         }
         
         if(this.is("input[type='radio']")) {
 			attachToElement = this.next();
 		}
+		
 		else if(this.is("button")) {
 			attachToElement = this.parent();
 		}
